d41fba5ee07d0e76f238da564d3c40341767eca2
wowowo <158035249+wowowo1213@users.noreply.github.com>
2025-11-20 23:45:18 +0800

    2025-11-20 修改

--- src/components/Button/example.test.ts
+++ src/components/Button/example.test.ts
@@ -1,18 +1,17 @@
-const testFn = (number: number, callback: Function) => {
-  if (number > 10) {
-    callback(number);
-  }
-};
+import { test, describe } from 'vitest';
+import { mount } from '@vue/test-utils';
+import Button from './Button.vue';
 
-import { test, describe, vi, expect } from 'vitest';
-
-describe('functions', () => {
-  test('create a mock funtion', () => {
-    const callback = vi.fn();
-    testFn(12, callback);
-    expect(callback).toHaveBeenCalled(); // callback函数是否被调用
-    expect(callback).toHaveBeenCalledWith(12); // callback函数的参数是不是12
+describe('Button.vue', () => {
+  test('basic button', () => {
+    const wrapper = mount(Button, {
+      props: {
+        type: 'primary',
+      },
+      slots: {
+        default: 'button',
+      },
+    });
+    console.log(wrapper.html());
   });
-  test('spy on method', () => {});
-  test('mock thiird party module', () => {});
 });
